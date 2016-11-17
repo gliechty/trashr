74bf2c870c7b6f636416996b525a67f0a8da18fa
gliechty <guy.liechty@gmail.com>
2016-11-17 13:14:51 -0700

    it works! just kidding

--- game.js
+++ game.js
@@ -20,27 +20,62 @@ function mkTrash(){
 	var screen = document.getElementById("screen");
 	screen.appendChild(trash);
 }
+mkTrash();
 
 // creates trash at regular interval
-window.setInterval(function(){
-  mkTrash();
-  rmTrash();
-}, 5000);
+// window.setInterval(function(){
+//   mkTrash();
+// }, 3000);
 
-// remove trash from DOM
-// checks margin
+// function mvTrashDown(){
+// 	console.log("it's getting called");
+// 	var trashArray = document.getElementsByClassName('trash');
+// 	console.log(trashArray);
+// 	trash = trashArray[0];
+
+// 	// trash.style.display= "absolute";
+// 	var count=0;
+// 	console.log("counting "+count);
+// 	while (count<=5){
+// 		trash.style.marginTop = (count*100) +'px';
+// 		console.log(trash.style.marginTop);
+// 		count+=1;
+// 		console.log(count);
+// 		setTimeout(mvTrashDown, 1000);
+// 	}
+// }
+
+// (function move() {
+//     var character=document.getElementById("character");
+//     if(character.style.left<newx) {
+//         character.style.left += pxsecx;
+//         character.style.top += pxsecy;
+//         setTimeout(move, 1000);
+//     }
+// })();
+
+
+
+// window.setInterval(function(){
+// 	mvTrashDown();
+// }, 1000);
+
+
+// checks top margin to remove trash 
 function rmTrash() {
 	var trash = document.getElementsByTagName("div");
-	console.log("another one");
-	for (i=0; i<trash.length; i++){
-		console.log(trash[i].style.top);
-		// if (trash[i].style.top=="100%"){
-		// 	console.log(trash[i]);
-			// var parent=trash[i].parentNode;
-			// parent.removeChild(trash[i]);
-		}
-	// }
+	// var trash = document.getElementsByClassName("trash");
+	var trashItem = trash[0];
+	console.log(trashItem);
+	trashItem.addEventListener('webkitAnimationEnd', function(event){trashItem.style.display = 'none';});
 }
+
+// window.setInterval(function(){
+//   rmTrash();
+// }, 1000);
+
+
+// rmTrash();
 // function rmTrash(){
 // 	trashItems=document.getElementsByClassName('animated');
 // 	console.log(trashItems);
@@ -52,24 +87,24 @@ function rmTrash() {
 // }
 
 // manipulate animations with event listener
-// document.addEventListener('keydown', function(event) {
-//     //left
-//     if (event.keyCode === 37) {
-//         console.log('left');
-//         var trash = document.getElementsByTagName('div');
-//         console.log(trash);
-//         for (i=0; i<trash.length; i++){
-//         	trash[i].style.marginLeft -= "10px";
-//         }
-//     } else if (event.keyCode === 39) {
-//     	console.log('right');
-//     	var trash = document.getElementsByTagName('div');
-//     	console.log(trash);
-//     	for (i=0; i<trash.length; i++){
-//         	trash[i].style.marginLeft += "10px";
-//     	}
-// 	}
-// });
+
+document.addEventListener('keydown', function(event) {
+	console.log("working");
+    //left
+    // var trashArray = document.getElementsByClassName('trash');
+    // var trash=trashArray[0];
+    // console.log(trash);
+    if (event.keyCode === 37) {
+        console.log('left');
+        // console.log(trash.style.left);
+        // trash.style.marginLeft = "0px";
+        // console.log(trash.style.left);
+    } else if (event.keyCode === 39) {
+    	console.log('right');
+    	// console.log(trash.style.left);
+     //    trash.style.left = trash.style.left + 10 + "px";
+	}
+});
 
 // create win/pass condition
 
