c94b757421b757770ad67a697d5c82ff16ccf199
gliechty <guy.liechty@gmail.com>
2016-11-18 13:40:19 -0700

    adds messages, a timer, modals, and a win condition

--- game.js
+++ game.js
@@ -1,16 +1,6 @@
-// provide instructions to play (onload alert or other)
-
-// window.onload = function(){
-// 	setTimeout(function(){
-// 		alert("Welcome to Trashr\n\nUse your arrow keys to direct the trash to the bin.\n\nRecycle or be Sorry...");
-// 	}, 600);
-// };
-
-// button to start game
-
+// makes timer
 
 // creates trash @random position in container
-
 function mkTrash(){
 	var trash = document.createElement("div");
 	// trash.innerHTML=("trash");
@@ -24,15 +14,12 @@ function mkTrash(){
 mkTrash();
 
 // creates trash at regular interval
-
 window.setInterval(function(){
   mkTrash();
 }, 5000);
 
 
-// Checks if trashItems ![] 
 // removes first child if there are multiple
-
 function rmTrash(){
     var trashItems=document.getElementsByClassName('trash');
     var firstChild=trashItems[0];
@@ -43,53 +30,95 @@ function rmTrash(){
 }
 setInterval(function(){rmTrash();}, 50);
 
+// removes first message if there are multiple
+function rmMessage(){
+    var messages=document.getElementsByClassName('message');
+    var firstMessage=messages[0];
+    var screen=document.getElementById("screen");
+    screen.removeChild(firstMessage);
+}
+setInterval(function(){rmMessage();}, 5000);
+
 
 // manipulate animations with event listener
-
 document.addEventListener('keydown', function(event) {
     //left
     var trashArray = document.getElementsByClassName('trash');
-    console.log(trashArray);
-    for (i=0; i<trashArray.length; i++){
+    // console.log(trashArray);
         if (event.keyCode === 37) {
-            console.log('left');
-            // console.log(trash.style.marginLeft);
-            trashArray[i].style.marginLeft = (parseInt(trashArray[i].style.marginLeft)-2)+"%";
-            // console.log(trash.style.left);
+            // shows left margin for debugging initial drop
+            // console.log(trashArray[0].style.marginLeft);
+
+            trashArray[0].style.marginLeft = (parseInt(trashArray[0].style.marginLeft)-2)+"%";
         } else if (event.keyCode === 39) {
-        	console.log('right');
-            trashArray[i].style.marginLeft = (parseInt(trashArray[i].style.marginLeft)+2)+"%";
-    	}
+            // shows left margin for debugging intitial drop
+            // console.log(trashArray[0].style.marginLeft);
+
+            trashArray[0].style.marginLeft = (parseInt(trashArray[0].style.marginLeft)+2)+"%";
     }
 });
 
-// create win/pass condition
+// sets points and messages
 var points = 0;
 function chkPoints(){
     var trashArray = document.getElementsByClassName('trash');
-    console.log(trashArray[0]);
-    if (parseInt(trashArray[0].style.marginLeft)>34 && parseInt(trashArray[0].style.marginLeft)<41){
+    if (parseInt(trashArray[0].style.marginLeft)>=34 && parseInt(trashArray[0].style.marginLeft)<=41){
         points+=1;
-        console.log(points);
         document.getElementById("qty").value=points.toString();
+        if (points%2===0){
+            console.log("Noice!");
+            var message=document.createElement("div");
+            message.innerHTML="Noice!";
+            message.setAttribute('class', 'message');
+            message.className += ' fade';
+            var screen=document.getElementById("screen");
+            screen.appendChild(message);
+        } else if (points%5===0 && points>=5){
+            console.log("Baller!!");
+            var message=document.createElement("div");
+            message.innerHTML="Baller!";
+            message.setAttribute('class', 'message');
+            message.className += ' fade';
+            var screen=document.getElementById("screen");
+            screen.appendChild(message);
+        }
     } else {
         points-=1;
-        console.log(points);
         document.getElementById("qty").value=points.toString();
+        if (points%2===0){
+            console.log("what's wrong with you??");
+            var message=document.createElement("div");
+            message.innerHTML=("What's wrong with you?");
+            message.setAttribute('class', 'message');
+            message.className += ' fade';
+            var screen=document.getElementById("screen");
+            screen.appendChild(message);
+        } else if (points%3===0){
+            console.log("are you high??");
+            var message=document.createElement("div");
+            message.innerHTML=("Are You High?");
+            message.setAttribute('class', 'message');
+            message.className += ' fade';
+            var screen=document.getElementById("screen");
+            screen.appendChild(message);
+        }
     }
 }
 setInterval(function(){chkPoints();}, 5000);
 
+// launches a win or lose modal
 // function isWin(){
+//     if (points>=1){
+//         var winModal = document.getElementById("winModal");
+//         winModal.style.display = "block";
+//     } else {
+//         var loseModal = document.getElementById("loseModal");
+//         loseModal.style.display = "block";
+//     }
 
 // }
 
-// keep score
+// // sets length of game
+// setInterval(function(){isWin();}, 25000);
+// -----------
 
-// function addScore(){
-
-// }
-
-// create timer (link w/ start button)
-
-// create win/pass condition
